14c94f21671329601bee560623274e5c926d0e98
kevin <kevin>
2017-01-11 18:23:00 +0700

    update fixed table header

--- cement/imports/ui/reports/invoiceByItem.js
+++ cement/imports/ui/reports/invoiceByItem.js
@@ -44,7 +44,14 @@ indexTmpl.onCreated(function () {
     this.endDate = new ReactiveVar(moment().endOf('days').toDate());
     createNewAlertify('invoiceByItemReport');
     paramsState.set(FlowRouter.query.params());
+
 });
+indexTmpl.onRendered(function () {
+    Meteor.setTimeout(function () {
+        $("table").fixMe();
+    },2000)
+});
+
 indexTmpl.helpers({
     schema(){
         return invoiceSchema;
@@ -189,3 +196,33 @@ AutoForm.hooks({
         }
     }
 });
+//Copyright by kevin
+$.fn.fixMe= function() {
+    return this.each(function() {
+        var $this = $(this),
+            $t_fixed;
+        function init() {
+            $this.wrap('<div class="container-fix-header" />');
+            $t_fixed = $this.clone();
+            $t_fixed.find("tbody").remove().end().addClass("fixed").insertBefore($this);
+            resizeFixed();
+        }
+        function resizeFixed() {
+            $t_fixed.find("th").each(function(index) {
+                $(this).css("width",$this.find("th").eq(index).outerWidth()+"px");
+            });
+        }
+        function scrollFixed() {
+            var offset = $(this).scrollTop(),
+                tableOffsetTop = $this.offset().top,
+                tableOffsetBottom = tableOffsetTop + $this.height() - $this.find("thead").height();
+            if(offset < tableOffsetTop || offset > tableOffsetBottom)
+                $t_fixed.hide();
+            else if(offset >= tableOffsetTop && offset <= tableOffsetBottom && $t_fixed.is(":hidden"))
+                $t_fixed.show();
+        }
+        $(window).resize(resizeFixed);
+        $(window).scroll(scrollFixed);
+        init();
+    });
+}
